5473ea1fa4fbc4b7be09eee3b1a07410f13a40f0
expandsource <expandsource@gmail.com>
2023-11-21 14:13:28 +0900

    로그아웃 함수 헤더 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,17 @@ import { useAuth } from './security/AuthContext';
 
 function Header() {
   // 컨텍스트에서 인증 정보를 받아와서 인증여부에 따라 렌더링을 다르게 함.
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, logout } = useAuth();
   return (
     <div>
       {!isAuthenticated && <Link to='/login'>로그인</Link>}
       {!isAuthenticated && <Link to='/join'>회원가입</Link>}
       {isAuthenticated && <Link to='/myPage'>회원정보</Link>}
-      {isAuthenticated && <Link to='/logout'>로그아웃</Link>}
+      {isAuthenticated && (
+        <Link to='/logout' onClick={logout}>
+          로그아웃
+        </Link>
+      )}
     </div>
   );
 }
